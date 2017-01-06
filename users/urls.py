6013798088fbfe7144a1e98b3eaff41563209886
eclipsenn <eclipseinn@mail.ru>
2017-01-06 03:43:10 +0300

    remove redundant view

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns('',
     url(r'^login/$', 'users.views.login', name='login'),
     url(r'^logout/$', 'users.views.logout', name='logout'),
     url(r'^register/$', 'users.views.register', name='register'),
-    url(r'^register/complete/$', 'users.views.complete_registration', name='complete_reg'),
     url(r'^profile/$', TemplateView.as_view(template_name='profile.html'), name='profile'),
     url('^', include('django.contrib.auth.urls')),
 )
